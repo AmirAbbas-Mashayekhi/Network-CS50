9a3d4c56cbc31dd21c8cf96cd3770ed40a7ff01a
AmirAbbas Mashayekhi <AmirAbbasMashayekhi.dev@gmail.com>
2024-12-16 02:04:47 +0330

    Add admin panel registeration for models.

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import User, Post, Follow, Like
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "email", "is_active", "date_joined"]
+    search_fields = ["username"]
+    list_per_page = 10
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["user", "short_body", "created_at"]
+    search_fields = ["username__istartswith", "short_body__istartswith"]
+    autocomplete_fields = ["user"]
+    list_per_page = 10
+
+    def short_body(self, post):
+        return post.body[:30] + "..."
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ["follower", "followed"]
+    search_fields = ["follower__user", "following__user"]
+    autocomplete_fields = ["follower", "followed"]
+    list_per_page = 10
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ["user", "post", "created_at"]
+    autocomplete_fields = ["post", "user"]
+    list_per_page = 10
