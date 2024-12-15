7b113cb03f37c665c814cddeec598f247c27da34
AmirAbbas Mashayekhi <AmirAbbasMashayekhi.dev@gmail.com>
2024-12-16 01:15:05 +0330

    Implement models

--- network/models.py
+++ network/models.py
@@ -3,4 +3,63 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    bio = models.CharField(max_length=300)
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="following"
+    )
+    followed = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="followers"
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["follower", "followed"], name="unique_follow_relationship"
+            )
+        ]
+        indexes = [
+            models.Index(fields=["follower"]),
+            models.Index(fields=["followed"]),
+        ]
+
+    def __str__(self):
+        return f"{self.follower_user} follows {self.followed_user}"
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["user"]),
+            models.Index(fields=["-created_at"]),
+        ]
+
+    def __str__(self):
+        return f"{self.user.username}'s Post: {self.body[:30]}"
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "post"], name="unique_like_relationship"
+            )
+        ]
+
+        indexes = [
+            models.Index(fields=["user"]),
+            models.Index(fields=["post"]),
+        ]
+
+    def __str__(self):
+        return f"{self.user.username} likes {self.post.id}"
