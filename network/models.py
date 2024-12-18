41ba48ef6a4f5968dbb39045f535eb6fba3cd243
AmirAbbas Mashayekhi <AmirAbbasMashayekhi.dev@gmail.com>
2024-12-18 04:09:02 +0330

    Add related names

--- network/models.py
+++ network/models.py
@@ -45,8 +45,8 @@ class Post(models.Model):
 
 
 class Like(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="liked")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
